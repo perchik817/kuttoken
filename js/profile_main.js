ae38dcef752edc2fedeed40db50ed95bdcdc156d
Perchik <saikalrustamovna@gmail.com>
2024-01-31 01:16:03 +0600

    for adap new css

--- js/profile_main.js
+++ js/profile_main.js
@@ -22,34 +22,14 @@ window.addEventListener('resize', handleResize);
 
 window.addEventListener('load', updateContent);
 
-document.addEventListener("DOMContentLoaded", function() {
-    var lastScrollTop = 0;
-    var header = document.querySelector('.header');
-
-    window.addEventListener("scroll", function(){
-        var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-
-        if (currentScroll <= 0) {
-            // Верх страницы
-            header.style.position = 'absolute';
-            header.style.backgroundColor = 'transparent'; // Сделать шапку прозрачной
-            header.style.zIndex = "1";
-        } else if (currentScroll > lastScrollTop){
-            // Прокрутка вниз
-            header.style.position = 'absolute';
-            header.style.backgroundColor = 'transparent'; // Вернуть исходный цвет фона
-        } else {
-            // Прокрутка вверх
-            header.style.position = 'fixed';
-            header.style.top = '0';
-            header.style.width = '100%';
-            header.style.backgroundColor = '#0A0A0A';
-            header.style.zIndex = "1000";
-        }
-
-        lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // Обновить lastScrollTop к текущему положению скролла
-    });
-});
+function toggleSidebar() {
+    let sidebar = document.getElementById('sidebar');
+    if (sidebar.style.width === '100%') {
+      sidebar.style.width = '0';
+    } else {
+      sidebar.style.width = '100%';
+    }
+  }
 
 
 //cover change popup
